7cb4a0535a097671d10c407a238422bc98dfcfef
KARAN KUMAR <Karanyadav3775@gmail.com>
2025-08-04 21:54:22 +0530

    Prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,17 @@ const jobRoutes = require("./routes/jobs");
 const app = express();
 
 // Middleware
-app.use(cors()); // Allow frontend requests
-app.use(express.json()); // Parse JSON bodies
+app.use(cors()); 
+app.use(express.json()); 
 
-// MongoDB Connection
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log(" Connected to MongoDB"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
 // Routes
 app.use("/api/jobs", jobRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
